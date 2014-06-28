e2b959230a469bcefb28417a14f287fdd6a80efb
Alex Shakespeare <alex@shakso.com>
2014-06-28 21:53:00 +0100

    added stuf

--- js/index.js
+++ js/index.js
@@ -16,19 +16,74 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var cb = {};
+        
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+
+        cb.webdb={};
+        cb.webdb.db = null;
+
+        cb.webdb.open = function() {
+          var dbSize = 5 * 1024 * 1024; // 5MB
+          cb.webdb.db = openDatabase("cb", "1", "cb", dbSize);
+        };
+
+        cb.webdb.onError = function(tx, e) {
+          alert("There has been an error: " + e.message);
+        };
+
+        cb.webdb.onSuccess = function(tx, r) {
+          // re-render the data.
+          // loadTodoItems is defined in Step 4a
+          //
+        };
+
+        cb.webdb.createTable = function() {
+            var db = cb.webdb.db;
+            db.transaction(function(tx) {
+                tx.executeSql("CREATE TABLE IF NOT EXISTS " +
+                              "passenger(ID INTEGER PRIMARY KEY ASC, code TEXT, firstName TEXT, lastName TEXT, seat INTEGER, booked_on DATETIME, checkin_on DATETIME)", []);
+                });
+            db.transaction(function(tx) {
+                tx.executeSql("DELETE FROM passenger",[]);
+            });
+
+        };
+
+        cb.webdb.add = function(code, firstName, lastName, seat) {
+          var db = cb.webdb.db;
+          db.transaction(function(tx){
+            tx.executeSql("INSERT INTO passenger(code, firstName, lastName, seat) VALUES (?,?,?,?)",
+                [code, firstName, lastName, seat],
+                cb.webdb.onSuccess,
+                cb.webdb.onError);
+           });
+        };
+
+        cb.webdb.getPaxList = function(renderFunc, code) {
+            var db = cb.webdb.db;
+            db.transaction(function(tx) {
+                tx.executeSql("SELECT * FROM passenger WHERE code=?", [code], renderFunc,
+                cb.webdb.onError);
+            });
+        };
+
+        $.getJSON( "http://shakso.com/paxList.htm", function( data ) {
+            cb.webdb.open();
+            cb.webdb.createTable();
+            $.each( data.paxList, function( key, val ) {
+                cb.webdb.add(val.code, val.firstName, val.lastName, val.seat);
+            });
+        });
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // `load`, `deviceready`, `offline`, and `online`.
+
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.getElementById('scan').addEventListener('click', this.scan, false);
-        document.getElementById('encode').addEventListener('click', this.encode, false);
+        $('#scan').click(this.scan);
     },
 
     // deviceready Event Handler
@@ -39,30 +94,33 @@ var app = {
         app.receivedEvent('deviceready');
     },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    scan: function() {
+        try {    
+            var scanner = cordova.require("cordova/plugin/BarcodeScanner");
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+            scanner.scan( function (result) { 
+                app.getPax(result.text);
+            }, function (error) { 
+                alert("Scanning failed: ", error); 
+            });
+        } catch (e) {
+            app.getPax('AAAAA');
+        }
     },
 
-    scan: function() {
-        console.log('scanning');
-        
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+    getPax: function(code) {
 
-        scanner.scan( function (result) { 
+        tableOut="<table border=1>";
 
-            $("#result").html("Result: " + result.text ); 
-            
+        cb.webdb.getPaxList(function(tx, rs) {
+            for (var i=0; i < rs.rows.length; i++) {
+                console.log(rs);
+                tableOut=tableOut + "<tr><td>" + rs.rows.item(i).firstName + "</td><td>"+ rs.rows.item(i).lastName + "</td><td>"+ rs.rows.item(i).seat + "</td></tr>";
+            }
+            tableOut=tableOut + "</table>";
 
-        }, function (error) { 
-            console.log("Scanning failed: ", error); 
-        } );
+            $("#result").html(tableOut); 
+            console.log(tableOut);
+        }, code);       
     }
 };
