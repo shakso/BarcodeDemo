026dbc11a5de18db031a68e535751d297147ddf6
Alex Shakespeare <alex@shakso.com>
2014-06-28 20:22:22 +0100

    Added push to html

--- js/index.js
+++ js/index.js
@@ -58,22 +58,8 @@ var app = {
 
         scanner.scan( function (result) { 
 
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
-
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
-            console.log(result);
-            /*
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-            }
-            */
+            $("#result").html("Result: " + result.text ); 
+            
 
         }, function (error) { 
             console.log("Scanning failed: ", error); 
